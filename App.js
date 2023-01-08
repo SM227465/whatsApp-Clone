13509f8a4d18b05b983b4621bb4d5b3fd165c6c5
Sourav Mandal <sm227465@gmail.com>
2023-01-08 12:15:31 +0530

    initial base

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaProvider style={styles.container}>
+      <SafeAreaView>
+        <Text style={styles.label}>Hello World!</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  label: {
+    color: 'black',
+    fontSize: 18,
+  },
+});
